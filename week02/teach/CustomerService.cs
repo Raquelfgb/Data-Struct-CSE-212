ddde269d3fcdff8e5fac1dac3ac320535a46163a
Raquelfgb <kelkinha@hotmail.com>
2024-09-26 10:40:29 -0600

    Team w2

--- week02/teach/CustomerService.cs
+++ week02/teach/CustomerService.cs
@@ -11,24 +11,76 @@ public class CustomerService {
         // Test Cases
 
         // Test 1
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: Test to see if we can add one customer and then serve the customer
+        // Expected Result: This should display the customer that was added
         Console.WriteLine("Test 1");
+        var service = new CustomerService(4);
+        service.AddNewCustomer();
+        service.ServeCustomer();
 
-        // Defect(s) Found: 
+        // Defect(s) Found: This found that the ServeCustomer should get the customer before deleting from the list
 
         Console.WriteLine("=================");
 
         // Test 2
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: Test to see if we can add two customers and then serve the customers in the right order
+        // Expected Result: This should display the customers in the same order that they were entered
         Console.WriteLine("Test 2");
+        service = new CustomerService(4);
+        service.AddNewCustomer();
+        service.AddNewCustomer();
+        Console.WriteLine($"Before serving customers: {service}");
+        service.ServeCustomer();
+        service.ServeCustomer();
+        Console.WriteLine($"After serving customers: {service}");
 
-        // Defect(s) Found: 
+        // Defect(s) Found: None
 
         Console.WriteLine("=================");
 
         // Add more Test Cases As Needed Below
+
+        // Test 3
+        // Scenario: Test to see if we can serve a customer if there is no customer
+        // Expected Result: This should display some error message
+        Console.WriteLine("Test 3");
+        service = new CustomerService(4);
+        service.ServeCustomer();
+
+        // Defect(s) Found: This found that I need to check the length in serve_customer and display an error message
+
+        Console.WriteLine("=================");
+
+        // Test 4
+        // Scenario: Does the max queue size get enforced?
+        // Expected Result: This should display some error message when the 5th one is added
+        Console.WriteLine("Test 4");
+        service = new CustomerService(4);
+
+        service.AddNewCustomer();
+        service.AddNewCustomer();
+        service.AddNewCustomer();
+        service.AddNewCustomer();
+        service.AddNewCustomer();
+
+        Console.WriteLine($"Service Queue: {service}");
+        // Defect(s) Found: This found that I need to do >= instead of > in AddNewCustomer
+        Console.WriteLine("=================");
+
+        // Test 5
+        // Scenario: Does the max size get defaulted to 10 if an invalid value is provided?
+        // Expected Result: It should display 10
+        Console.WriteLine("Test 5");
+
+        service = new CustomerService(0);
+        Console.WriteLine($"Size should be 10: {service}");
+
+        // Defect(s) Found: None
+
+        Console.WriteLine("=================");
+        
+
+
     }
 
     private readonly List<Customer> _queue = new();
@@ -88,9 +140,18 @@ public class CustomerService {
     /// Dequeue the next customer and display the information.
     /// </summary>
     private void ServeCustomer() {
-        _queue.RemoveAt(0);
-        var customer = _queue[0];
-        Console.WriteLine(customer);
+
+        if (_queue.Count <= 0)
+        {
+            Console.WriteLine("No customer in the queue");
+        }
+        else
+        {
+            var customer = _queue[0];
+            _queue.RemoveAt(0);
+            Console.WriteLine(customer);  
+        }
+        
     }
 
     /// <summary>
@@ -101,6 +162,6 @@ public class CustomerService {
     /// </summary>
     /// <returns>A string representation of the queue</returns>
     public override string ToString() {
-        return $"[size={_queue.Count} max_size={_maxSize} => " + string.Join(", ", _queue) + "]";
+        return $"[size={_queue.Count} max_size={_maxSize} => " + String.Join(", ", _queue) + "]";
     }
 }
